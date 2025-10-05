2200e7e9267e1d2cf4d4b932647bc92bfafd8fd9
3irdo <sgvmoe@gmail.com>
2025-10-04 21:17:32 -0500

    github action astro ghub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'static'
+  output: 'static',
+  site:'https://github.com/3irdo',
 });
